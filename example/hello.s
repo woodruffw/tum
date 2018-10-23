1719277ba9fea1c61e6e8a5204a5f89aab1ca28b
William Woodruff <william@yossarian.net>
2018-10-22 21:12:02 -0400

    example: Add hello, world!

---
+++ example/hello.s
@@ -0,0 +1,31 @@
+; hello.s
+; hello world
+
+mov gp0, 0x68
+iow gp0
+mov gp0, 0x65
+iow gp0
+mov gp0, 0x6c
+iow gp0
+mov gp0, 0x6c
+iow gp0
+mov gp0, 0x6f
+iow gp0
+mov gp0, 0x2c
+iow gp0
+mov gp0, 0x20
+iow gp0
+mov gp0, 0x77
+iow gp0
+mov gp0, 0x6f
+iow gp0
+mov gp0, 0x72
+iow gp0
+mov gp0, 0x6c
+iow gp0
+mov gp0, 0x64
+iow gp0
+mov gp0, 0x21
+iow gp0
+mov gp0, 0x0a
+iow gp0
